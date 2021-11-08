9b77eca9835e8e8b9ddbb0027ebc66070612545a
nicomellon <nmellon94@gmail.com>
2021-11-08 18:03:18 +0100

    labs done

--- javascript/index.js
+++ javascript/index.js
@@ -1,37 +1,92 @@
-// This will print in the wrong order.
-// We added it as an example and to test that the arrays from data.js are loaded
-
-// 🚨🚨🚨 Comment out the below code before you start working on the code
-
-// Out of sync
-  getInstruction("mashedPotatoes", 0, (step1) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 1, (step2) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 2, (step3) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 3, (step4) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
-  }, (error) => console.log(error));
-  
-  getInstruction("mashedPotatoes", 4, (step5) => {
-    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step5}</li>`;
-    document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
-  }, (error) => console.log(error));
-
-
-
 // Iteration 1 - using callbacks
-// ...
+const mashedPotatoOl = document.querySelector("#mashedPotatoes");
+const mashedPotatoImg = document.querySelector("#mashedPotatoesImg");
+
+getInstruction("mashedPotatoes", 0, (step0) => {
+  const instructionLi = document.createElement("li");
+  instructionLi.innerText = step0;
+  mashedPotatoOl.appendChild(instructionLi);
+
+  getInstruction("mashedPotatoes", 1, (step1) => {
+    const instructionLi = document.createElement("li");
+    instructionLi.innerText = step1;
+    mashedPotatoOl.appendChild(instructionLi);
+
+    getInstruction("mashedPotatoes", 2, (step2) => {
+      const instructionLi = document.createElement("li");
+      instructionLi.innerText = step2;
+      mashedPotatoOl.appendChild(instructionLi);
+
+      getInstruction("mashedPotatoes", 3, (step3) => {
+        const instructionLi = document.createElement("li");
+        instructionLi.innerText = step3;
+        mashedPotatoOl.appendChild(instructionLi);
+
+        getInstruction("mashedPotatoes", 4, (step4) => {
+          const instructionLi = document.createElement("li");
+          instructionLi.innerText = step4;
+          mashedPotatoOl.appendChild(instructionLi);
+
+          const readyLi = document.createElement("li");
+          readyLi.innerText = "Mashed potatoes are ready!";
+          mashedPotatoOl.appendChild(readyLi);
+
+          mashedPotatoImg.removeAttribute("hidden");
+        });
+      });
+    });
+  });
+});
 
 // Iteration 2 - using promises
-// ...
+const steakOl = document.querySelector("#steak");
+const steakImg = document.querySelector("#steakImg");
+
+// function to build new li rows
+const newInstructionLi = (innerText) => {
+  const steakLi = document.createElement("li");
+  steakLi.innerText = innerText;
+  steakOl.appendChild(steakLi);
+};
+
+obtainInstruction("steak", 0)
+  .then((instruction) => {
+    newInstructionLi(instruction);
+    return obtainInstruction("steak", 1);
+  })
+  .then((instruction) => {
+    newInstructionLi(instruction);
+    return obtainInstruction("steak", 2);
+  })
+  .then((instruction) => {
+    newInstructionLi(instruction);
+    return obtainInstruction("steak", 3);
+  })
+  .then((instruction) => {
+    newInstructionLi(instruction);
+    return obtainInstruction("steak", 4);
+  })
+  .then((instruction) => {
+    newInstructionLi(instruction);
+    return obtainInstruction("steak", 5);
+  })
+  .then((instruction) => {
+    newInstructionLi(instruction);
+    return obtainInstruction("steak", 6);
+  })
+  .then((instruction) => {
+    newInstructionLi(instruction);
+    return obtainInstruction("steak", 7);
+  })
+  .then((instruction) => {
+    newInstructionLi(instruction);
+
+    const enjoyLi = document.createElement("li");
+    enjoyLi.innerText = "Steak is ready!";
+    steakOl.appendChild(enjoyLi);
+
+    steakImg.removeAttribute("hidden");
+  });
 
 // Iteration 3 using async/await
 // ...
